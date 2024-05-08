b76d64e1f9e9bf047628f5615930b9a9eabe68c9
Waheed Khaled <waheedkhaled95@gmail.com>
2024-05-08 22:30:05 +0000

    Test

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Script to fix all terminations of php.
+
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+}
